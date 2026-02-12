97ad1b1128748345b408ddb8b3c5dfc7b639fdb3
jaydeelew <jaydeelew@gmail.com>
2026-02-12 12:46:44 -0600

    Remove desktop-specific line break from the welcome modal and simplify the text structure for better responsiveness.

--- frontend/src/components/tutorial/TutorialWelcomeModal.tsx
+++ frontend/src/components/tutorial/TutorialWelcomeModal.tsx
@@ -71,7 +71,7 @@ export const TutorialWelcomeModal: React.FC<TutorialWelcomeModalProps> = ({
           </div>
           <p className="tutorial-welcome-more-features">
             Many more features are available!
-            <br className="tutorial-welcome-line-break-desktop" />
+            <br />
             Check out our{' '}
             <Link to="/how-it-works" className="tutorial-welcome-link">
               How it works page
